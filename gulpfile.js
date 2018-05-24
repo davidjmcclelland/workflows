03211b20637842cf6ade0bea1e59f5062ee6d656
dmcclelland <dmcclelland@ptc.com>
2018-05-24 12:37:28 -0400

    added gulp concatenation task

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
-    // tell node to bring in the gulp library with a require statement
-    // npm install sudo npm install --save-dev gulp
+// tell node to bring in the gulp library with a require statement
+// npm install sudo npm install --save-dev gulp
 var gulp = require('gulp'),
     // npm install sudo npm install --save-dev gulp-util
     gutil = require('gulp-util'),
     // npm install sudo npm install --save-dev gulp-coffee
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    // npm install sudo npm install --save-dev gulp-concat
+    concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
 //var coffeeSources = ['*.coffee'];
+
+var jsSources = [
+    'components/scripts/rclick.js',
+    'components/scripts/pixgrid.js',
+    'components/scripts/tagline.js',
+    'components/scripts/template.js'
+];
 
 // add method with arbitrary name for gulp to perform
 // enter gulp log in terminal at project root to execute
@@ -18,9 +27,16 @@ gulp.task('log', function () {
 // transpile, copy over and rename coffee script file to js
 gulp.task('coffee', function () {
     gulp.src(coffeeSources)
-        .pipe(coffee({ bare: true })
+        .pipe(coffee({bare: true})
             .on('error', gutil.log))
         .pipe(gulp.dest('components/scripts'))
+});
+
+// transpile, copy over and rename coffee script file to js
+gulp.task('concat', function () {
+    gulp.src(jsSources)
+        .pipe(concat('script.js'))
+            .pipe(gulp.dest('builds/development/js'))
 });
 
 /*
